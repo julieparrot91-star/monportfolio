8861e17f12f394566d9a39cd89814add327749d5
julienparrot <julien.parrot22@gmail.com>
2026-03-14 14:17:04 +0100

    link certification

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -14,7 +14,7 @@ export const certifications: Certification[] = [
 	{
 		name: "Microsoft Certified: Azure Administrator Associate",
 		issuer: "Microsoft",
-		credlyUrl: "https://www.credly.com/badges/your-credly-link",
+		credlyUrl: "https://learn.microsoft.com/api/credentials/share/fr-fr/julienparrot-2391/62F5DB12CAD5DB9A?sharingId=E44D1F0F723FACE2",
 		dateIssued: "9 mars 2026",
 		expirationDate: "10 mars 2027",
 		badgeImageUrl: az104Badge.src,
